489e16f84e5513730d01240b9a76647fb9fa4795
Tobias Selva <tobiasselva1@gmail.com>
2021-08-04 19:33:09 -0300

    subiendo pagina web al repo remoto!

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>
+{
+//console.log("click!");
+//del elemento nav-menu vamos a añadir una clase llamada show y con el metodo toggle se añade 
+// o se quita segun si ya lo tiene
+    
+    console.log(document.querySelector('.nav-menu').classList.toggle('show'));
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-news', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
